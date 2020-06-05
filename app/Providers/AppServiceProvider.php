bf50f2975aca9651143ab4b5fd9872c90777c918
jingye <jingye@xmu.edu.cn>
2020-06-05 09:06:39 +0800

    edit gitignore and appService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //mysql 5.6
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
