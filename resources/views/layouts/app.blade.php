fac57cec2afd71411e79357262efc6be4d7d8e2b
jingye <jingye@xmu.edu.cn>
2020-08-03 10:36:41 +0800

    edit title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SUMMER SCHOOL') }}</title>
+    <title>SUMMER SCHOOL</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" ></script>
